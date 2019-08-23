b984dfc896d6276ce592a4cfd663e007e8a814bf
Pablo Sousa <pablosousa.ads@gmail.com>
2019-08-22 21:51:28 -0300

    adicionado softDelete e modificado id para uuid

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    protected $fillable = [
-        'name',
-        'description',
-        'is_active'
-    ];
+    use SoftDeletes;
+    use Uuid;
+
+    protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = ['id' => 'string'];
 }
