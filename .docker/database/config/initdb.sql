9d35717f15166cd3665e5c1c43061ac1812858ac
Pablo Sousa <pablosousa.ads@gmail.com>
2019-08-24 19:31:46 -0300

    adicionado .env de teste, modificado initdatabase do mysql, para criar o banco de dados da app e test; atualizado docker-compose

---
+++ .docker/database/config/initdb.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS codeflix_videos;
+CREATE DATABASE IF NOT EXISTS codeflix_videos_test;
--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Alterando permissao da pasta storage...."
+echo "Copiando .env da aplicacao..."
+dockerize -template ./.docker/php/.env:.env
+
+echo "Copiando .env do ambiente de teste..."
+dockerize -template ./.docker/php/.env.testing:.env.testing
+
+echo "Aguardando conexao com o banco de dados..."
+dockerize -wait tcp://db:3306 -timeout 60s
+
+echo "Alterando permissao da pasta storage..."
 chmod -R 775 storage/
 
 echo "Instalando dependencias php...."
