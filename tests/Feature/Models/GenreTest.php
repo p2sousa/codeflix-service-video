2e346861cdca2ebf257c5a3b03fc6a70916a5546
Pablo Sousa <pablosousa.ads@gmail.com>
2019-09-08 22:29:36 -0300

    criado teste de integracao CRUD da model Genre

---
+++ tests/Feature/Models/GenreTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Genre;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Traits\Uuid;
+
+class GenreTest extends TestCase
+{
+    use DatabaseMigrations;
+    use Uuid;
+
+    public function testList()
+    {
+        factory(Genre::class, 1)->create();
+        $genres = Genre::all();
+        $this->assertCount(1, $genres);
+        $genreKey = array_keys($genres->first()->getAttributes());
+        $this->assertEqualsCanonicalizing(
+            [
+                'id',
+                'name',
+                'is_active',
+                'created_at',
+                'updated_at',
+                'deleted_at',
+            ],
+            $genreKey
+        );
+    }
+
+    public function testCreate()
+    {
+        $genre = Genre::create([
+            'name' => 'teste1'
+        ]);
+
+        $genre->refresh();
+
+        $this->assertTrue($this->isUuid($genre->id));
+        $this->assertEquals('teste1', $genre->name);
+        $this->assertTrue($genre->is_active);
+
+        $genre = Genre::create([
+            'name' => 'teste1',
+            'is_active' => false
+        ]);
+
+        $this->assertFalse($genre->is_active);
+
+        $genre = Genre::create([
+            'name' => 'teste1',
+            'is_active' => true
+        ]);
+
+        $this->assertTrue($genre->is_active);
+    }
+
+    public function testUpdate()
+    {
+        $genre = factory(Genre::class)->create([
+            'is_active' => false,
+        ])->first();
+
+        $data = [
+            'name' => 'test_name_updated',
+            'is_active' => true,
+        ];
+
+        $genre->update($data);
+
+        foreach ($data as $key => $value) {
+            $this->assertEquals($value, $genre->{$key});
+        }
+    }
+
+    public function testDelete()
+    {
+        $genre = factory(Genre::class)->create()->first();
+
+        $genre->delete();
+
+        $this->assertNotNull($genre->deleted_at);
+        $this->assertNotNull(Genre::onlyTrashed()->first());
+    }
+}
