82635bc47cc8d5c8a67908df608a663e29d8788b
Pablo Sousa <pablosousa.ads@gmail.com>
2019-09-08 22:06:17 -0300

    criada trait para testes de campo UUID

---
+++ tests/Traits/Uuid.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Tests\Traits;
+
+
+trait Uuid
+{
+    protected function isUuid($uuid)
+    {
+        return is_string($uuid) && (bool) preg_match('/^[a-f0-9]{8,8}-(?:[a-f0-9]{4,4}-){3,3}[a-f0-9]{12,12}$/i', $uuid);
+    }
+}
