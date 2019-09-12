b3ef2e88c9db2182602ab139bfc64be5740a8014
Pablo Sousa <pablosousa.ads@gmail.com>
2019-09-12 00:42:17 -0300

    criado teste para validacao dos dados passados para criacao e edicao de categoria

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Http\Controllers\Api;
 
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestResponse;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -30,5 +31,66 @@ class CategoryControllerTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJson($category->toArray());
+    }
+
+    public function testInvalidationRulePost()
+    {
+        $response = $this->json('POST', route('categories.store'), []);
+
+        $this->assertInvalidationNameRequired($response);
+
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => str_repeat('a', 256),
+            'is_active' => 'a'
+        ]);
+
+        $this->assertInvalidationNameMax($response);
+        $this->assertInvalidationIsActiveBoolean($response);
+    }
+
+    public function testInvalidationRulePut()
+    {
+        $category = factory(Category::class)->create();
+        $response = $this->json('PUT', route('categories.update', ['category' => $category->id]), []);
+
+        $this->assertInvalidationNameRequired($response);
+
+        $response = $this->json('PUT', route('categories.update', ['category' => $category->id]), [
+            'name' => str_repeat('a', 256),
+            'is_active' => 'a'
+        ]);
+
+        $this->assertInvalidationNameMax($response);
+        $this->assertInvalidationIsActiveBoolean($response);
+    }
+
+    protected function assertInvalidationNameRequired(TestResponse $response)
+    {
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name'])
+            ->assertJsonFragment([
+                \Lang::trans('validation.required', ['attribute' => 'name'])
+            ]);
+    }
+
+    protected function assertInvalidationNameMax(TestResponse $response)
+    {
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name'])
+            ->assertJsonFragment([
+                \Lang::trans('validation.max.string', ['attribute' => 'name', 'max' => 255])
+            ]);
+    }
+
+    protected function assertInvalidationIsActiveBoolean(TestResponse $response)
+    {
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['is_active'])
+            ->assertJsonFragment([
+                \Lang::trans('validation.boolean', ['attribute' => 'is active'])
+            ]);
     }
 }
