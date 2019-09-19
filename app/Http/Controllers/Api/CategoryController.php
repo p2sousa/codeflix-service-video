2bf84d17d3d6a402a7ba1f66c98315e8f1a9689a
Pablo Sousa <pablosousa.ads@gmail.com>
2019-09-19 00:08:30 -0300

    CategoryController e GenreController extend de BasicController

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,86 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
-/**
- * Class CategoryController
- * @package App\Http\Controllers\Api
- */
-class CategoryController extends Controller
+class CategoryController extends BasicController
 {
-    /**
-     * @var array
-     */
-    private $rules = [
-        'name' => 'required|max:255',
-        'is_active' => 'boolean'
-    ];
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Category[]|\Illuminate\Database\Eloquent\Collection
-     */
-    public function index()
+    protected function model()
     {
-        return Category::all();
+        return Category::class;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return mixed
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request)
+    protected function rulesStore()
     {
-        $this->validate($request, $this->rules);
-
-        $category = Category::create($request->all());
-        $category->refresh();
-        return $category;
+        return [
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'is_active' => 'boolean'
+        ];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param Category $category
-     * @return Category
-     */
-    public function show(Category $category)
+    protected function rulesUpdate()
     {
-        return $category;
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Category $category
-     * @return Category
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function update(Request $request, Category $category)
-    {
-        $this->validate($request, $this->rules);
-        $category->update($request->all());
-        return $category;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Category $category
-     * @return \Illuminate\Http\Response
-     * @throws \Exception
-     */
-    public function destroy(Category $category)
-    {
-        $category->delete();
-        return response()->noContent();
+        return [
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'is_active' => 'boolean'
+        ];
     }
 }
--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -2,82 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Genre;
 use Illuminate\Http\Request;
 
-class GenreController extends Controller
+class GenreController extends BasicController
 {
-    /**
-     * @var array
-     */
-    private $rules = [
-        'name' => 'required|max:255',
-        'is_active' => 'boolean'
-    ];
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    protected function model()
     {
-        return Genre::all();
+        return Genre::class;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     * 
-     * @param Request $request
-     * @return mixed
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request)
+    protected function rulesStore()
     {
-        $this->validate($request, $this->rules);
-        $genre = Genre::create($request->all());
-        $genre->refresh();
-        return $genre;
-
+        return [
+            'name' => 'required|max:255',
+            'is_active' => 'boolean'
+        ];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Genre $genre)
+    protected function rulesUpdate()
     {
-        return $genre;
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Genre $genre
-     * @return Genre
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function update(Request $request, Genre $genre)
-    {
-        $this->validate($request, $this->rules);
-        $genre->update($request->all());
-        return $genre;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Genre $genre
-     * @return \Illuminate\Http\Response
-     * @throws \Exception
-     */
-    public function destroy(Genre $genre)
-    {
-        $genre->delete();
-        return response()->noContent();
+        return [
+            'name' => 'required|max:255',
+            'is_active' => 'boolean'
+        ];
     }
 }
