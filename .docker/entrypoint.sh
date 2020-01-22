42e91855e939c53527c89b48d043363f6f912cf8
Pablo Sousa <pablosousa.ads@gmail.com>
2020-01-21 22:49:29 -0300

    criado pasta backend e movido conteudo do laravel para ela.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 echo "Copiando .env da aplicacao..."
-dockerize -template ./.docker/php/.env:.env
+dockerize -template ./.docker/php/.env:backend/.env
 
 echo "Copiando .env do ambiente de teste..."
-dockerize -template ./.docker/php/.env.testing:.env.testing
+dockerize -template ./.docker/php/.env.testing:backend/.env.testing
 
 echo "Aguardando conexao com o banco de dados..."
 dockerize -wait tcp://db:3306 -timeout 60s
+
+cd backend
 
 echo "Alterando permissao da pasta storage..."
 chmod -R 775 storage/
