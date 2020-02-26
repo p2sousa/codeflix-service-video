ea99d49f2dfffb03478b65b2836b63368a50e718
Pablo Sousa <pablosousa.ads@gmail.com>
2020-02-26 12:36:44 -0300

    adicionado menu ao navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,26 +1,51 @@
-import * as React from 'react';
-import {AppBar, Button, Toolbar, Typography} from "@material-ui/core";
-import logo from '../../static/img/logo.png';
+import * as React from "react";
+import {AppBar, Button, IconButton, Menu, MenuItem, Theme, Toolbar, Typography} from "@material-ui/core";
+import logo from "../../static/img/logo.png";
 import {makeStyles} from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
         backgroundColor: '#000000'
     },
     title: {
         flexGrow: 1,
         textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')] : {
+            width: 170,
+        }
     }
-});
+}));
 
 export const Navbar: React.FC = () => {
     const classes = useStyles();
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge={'start'}
+                    color={'inherit'}
+                    aria-label={'open drawer'}
+                    aria-controls={'menu-appbar'}
+                    aria-haspopup={'true'}
+                >
+                    <MenuIcon/>
+                </IconButton>
+
+                <Menu
+                    id={'menu-appbar'}
+                    open={false}
+                >
+                    <MenuItem>Categorias</MenuItem>
+                </Menu>
+
                 <Typography className={classes.title}>
-                    <img src={logo} alt="CodeFlix"/>
+                    <img src={logo} alt="CodeFlix" className={classes.logo}/>
                 </Typography>
+
                 <Button color={'inherit'}>Login</Button>
             </Toolbar>
         </AppBar>
