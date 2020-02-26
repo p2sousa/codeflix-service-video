70e7b6be78f9da1131fbf301939abbf3f2e9263c
Pablo Sousa <pablosousa.ads@gmail.com>
2020-02-25 23:28:20 -0300

    adicionado material-ui ao front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
 
 const App = () => {
   return (
@@ -8,7 +9,9 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Button color={'primary'}>
+            Texto
+          </Button>
         </p>
         <a
           className="App-link"
