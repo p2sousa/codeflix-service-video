fc77b76c8bd75c6b6c030637faf51e616ffd1141
Pablo Sousa <pablosousa.ads@gmail.com>
2020-02-26 11:52:04 -0300

    criado componente Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Button} from "@material-ui/core";
+import {Navbar} from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Button color={'primary'}>
-            Texto
-          </Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar></Navbar>
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {AppBar, Button, Toolbar, Typography} from "@material-ui/core";
+import logo from '../../static/img/logo.png';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    toolbar: {
+        backgroundColor: '#000000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    }
+});
+
+export const Navbar: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <AppBar>
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.title}>
+                    <img src={logo} alt="CodeFlix"/>
+                </Typography>
+                <Button color={'inherit'}>Login</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
