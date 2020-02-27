7e0933621769eb795a28c6b9a21237d3e49a7a17
Pablo Sousa <pablosousa.ads@gmail.com>
2020-02-26 21:12:08 -0300

    criado mapa de rotas

---
+++ frontend/src/pages/category/List.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Page} from "../../components/Page";
+
+interface ListProps {
+
+}
+
+const List = (props: ListProps) => {
+    return (
+        <Page title={'Listagem de categorias'}/>
+    );
+};
+
+export default List;
